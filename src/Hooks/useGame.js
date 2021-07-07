3004178385023c3478ca2dc39887f30b4a599751
Linlin Li <paris1900lll@gmail.com>
2021-07-07 21:33:42 +0100

    need rotate

--- src/Hooks/useGame.js
+++ src/Hooks/useGame.js
@@ -3,6 +3,7 @@ import create from "zustand";
 const useGame = create((set, get) => ({
   gameOver: false,
   stopGame: () => set({ gameOver: true }),
+  startGame: () => set({ gameOver: false }),
 }));
 
 export default useGame;
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import createGrids from "../helper";
 import useSwitch from "../Hooks/mode";
@@ -14,40 +14,13 @@ export default function GameStart() {
   });
 
   const gameOver = useGame((store) => store.gameOver);
-
+  const stopGame = useGame((store) => store.stopGame);
+  const startGame = useGame((store) => store.startGame);
   const [piece, setPiece] = useState(randomTetriminos());
   const [board, setBoard] = useState(createGrids());
+  const [nextPiece, SetPiece] = useState(randomTetriminos());
 
-  useEffect(() => {
-    let clearId = setInterval(() => {
-      updatePiecePos(0, 1);
-    }, 1000);
-    return () => {
-      clearInterval(clearId);
-    };
-  }, []);
-
-  function restart() {
-    setPiece(randomTetriminos());
-    setBoard(createGrids());
-  }
-
-  function willCollide(newX, newY) {
-    for (let y = 0; y < piece.shape.length; y++) {
-      for (let x = 0; x < piece.shape[0].length; x++) {
-        //check we are inside the game boarder, check we are actually on the cell,check we are gonna clear the bottom or not
-        if (piece.shape[y][x] !== "") {
-          if (!board[y + piece.y + newY][x + piece.x + newX]) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
-  }
-  console.log(piece);
-
-  function placeOnBoard() {
+  const placeOnBoard = useCallback(() => {
     let newBoard = JSON.parse(JSON.stringify(board));
     for (
       let yRelativeToPiece = 0;
@@ -67,16 +40,93 @@ export default function GameStart() {
         }
       }
     }
+    if (newBoard[0].some((ele) => ele !== "")) {
+      console.log("stop");
+      stopGame();
+      return;
+    }
+
+    newBoard = newBoard.filter((row) => row.some((cell) => cell === ""));
+    let gap = board.length - newBoard.length;
+    for (let i = 0; i < gap; i++) {
+      newBoard.unshift(Array(12).fill(""));
+    }
+
     setBoard(newBoard);
     setPiece(randomTetriminos());
+  }, [piece.shape, piece.x, piece.y, board, stopGame]);
+
+  const willCollide = useCallback(
+    (newX, newY, piece) => {
+      if (!gameOver) {
+        for (
+          let yRelativeToPiece = 0;
+          yRelativeToPiece < piece.shape.length;
+          yRelativeToPiece++
+        ) {
+          for (
+            let xRelativeToPiece = 0;
+            xRelativeToPiece < piece.shape[0].length;
+            xRelativeToPiece++
+          ) {
+            //check we are inside the game boarder, check we are actually on the cell,check we are gonna clear the bottom or not
+            if (piece.shape[yRelativeToPiece][xRelativeToPiece] !== "") {
+              let absoluteBoardX = piece.x + xRelativeToPiece;
+              let absoluteBoardY = piece.y + yRelativeToPiece;
+              if (
+                board[absoluteBoardY + newY] === undefined ||
+                board[absoluteBoardY + newY][absoluteBoardX + newX] ===
+                  undefined ||
+                board[absoluteBoardY + newY][absoluteBoardX + newX] !== ""
+              ) {
+                return true;
+              }
+            }
+          }
+        }
+        return false;
+      }
+    },
+    [piece.shape, piece.x, piece.y, board, gameOver]
+  );
+
+  const updatePiecePos = useCallback(
+    (newX, newY) => {
+      if (!gameOver) {
+        if (!willCollide(newX, newY, piece)) {
+          setPiece((piece) => ({
+            ...piece,
+            x: piece.x + newX,
+            y: piece.y + newY,
+          }));
+        } else {
+          if (newY > 0) {
+            placeOnBoard();
+          }
+        }
+        return null;
+      }
+    },
+    [willCollide, gameOver, placeOnBoard]
+  );
+
+  useEffect(() => {
+    let clearId = setInterval(() => {
+      updatePiecePos(0, 1);
+    }, 1000);
+    console.log(clearId);
+    return () => {
+      clearInterval(clearId);
+    };
+  }, [updatePiecePos]);
+
+  function restart() {
+    startGame();
+    setPiece(randomTetriminos());
+    setBoard(createGrids());
   }
 
-  function updatePiecePos(newX, newY) {
-    if (!willCollide(newX, newY)) {
-      setPiece((piece) => ({ ...piece, x: piece.x + newX, y: piece.y + newY }));
-    }
-    return null;
-  }
+  console.log(piece);
 
   function movePiece(e) {
     if (!gameOver) {
@@ -92,9 +142,21 @@ export default function GameStart() {
     }
   }
 
-  function rotatePiece(piece, direction) {
+  function rotatePiece() {
     if (!gameOver) {
-      if (!willCollide()) {
+      let copyPiece = JSON.parse(JSON.stringify(piece));
+      let copyPieceShape = copyPiece.shape;
+      let rotatePiece = [];
+      for (let x = 0; x < copyPieceShape[0].length; x++) {
+        let row = [];
+        for (let y = 0; y < copyPieceShape.length; y++) {
+          row.unshift(copyPieceShape[y][x]);
+        }
+        rotatePiece.push(row);
+      }
+      console.log(rotatePiece);
+      if (!willCollide(0, 0, rotatePiece)) {
+        setPiece(rotatePiece);
       }
     }
     return;
@@ -164,9 +226,9 @@ export default function GameStart() {
         <h2
           className={`game-name ${
             mode === "Light" ? "light-gamename" : "dark-gamename"
-          }`}
+          } ${gameOver ? "game-over" : "Tetris"} `}
         >
-          Tetris
+          {` ${gameOver ? "" : "Tetris"}`}
         </h2>
         <section
           className={`control-panel ${
@@ -178,6 +240,7 @@ export default function GameStart() {
             className={`rotate ${
               mode === "Light" ? "light-rotate" : "dark-rotate"
             } `}
+            onClick={rotatePiece}
           >
             Rotate
           </button>
